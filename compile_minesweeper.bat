f35e61e8b2f4b944d075afbec1f3e52a05d67cc5
GrapefruitDoggo <ameliadurham@protonmail.com>
2021-04-03 17:19:14 +1100

    Updated compiler batch file

--- compile_minesweeper.bat
+++ compile_minesweeper.bat
@@ -1,22 +1,22 @@
-@del example.o
-@del example.nes
-@del example.map.txt
-@del example.labels.txt
-@del example.nes.ram.nl
-@del example.nes.0.nl
-@del example.nes.1.nl
-@del example.nes.dbg
+@del minesweeper.o
+@del minesweeper.nes
+@del minesweeper.map.txt
+@del minesweeper.labels.txt
+@del minesweeper.nes.ram.nl
+@del minesweeper.nes.0.nl
+@del minesweeper.nes.1.nl
+@del minesweeper.nes.dbg
 @echo.
 @echo Compiling...
-ca65 example.asm -g -o example.o
+ca65 minesweeper.asm -g -o minesweeper.o
 @IF ERRORLEVEL 1 GOTO failure
 @echo.
 @echo Linking...
-ld65 -o example.nes -C example.cfg example.o -m example.map.txt -Ln example.labels.txt --dbgfile example.nes.dbg
+ld65 -o minesweeper.nes -C minesweeper.cfg minesweeper.o -m minesweeper.map.txt -Ln minesweeper.labels.txt --dbgfile minesweeper.nes.dbg
 @IF ERRORLEVEL 1 GOTO failure
 @echo.
 @echo Generating FCEUX debug symbols...
-python example_fceux_symbols.py
+python minesweeper_fceux_symbols.py
 @echo.
 @echo Success!
 @pause
