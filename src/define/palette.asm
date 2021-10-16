a40e8fe83b6e1c4e190c9511866e03d6369568ae
GrapefruitDoggo <ameliadurham@protonmail.com>
2021-10-17 03:52:36 +1100

    fixed all the files having no newlines

--- src/define/palette.asm
+++ src/define/palette.asm
@@ -1,1 +1,12 @@
-.segment "ROMDATA"palette_background:.byte $0f, $28, $05, $25.byte $0f, $00, $10, $20.byte $0f, $0f, $0f, $0f.byte $0f, $0f, $0f, $0fpalette_sprites:.byte $0F, $05, $26, $28.byte $0F, $05, $09, $12.byte $0F, $01, $06, $1B.byte $10, $00, $30, $0F
+.segment "ROMDATA"
+palette_background:
+.byte $0f, $28, $05, $25
+.byte $0f, $00, $10, $20
+.byte $0f, $0f, $0f, $0f
+.byte $0f, $0f, $0f, $0f
+
+palette_sprites:
+.byte $0F, $05, $26, $28
+.byte $0F, $05, $09, $12
+.byte $0F, $01, $06, $1B
+.byte $10, $00, $30, $0F
--- src/main.asm
+++ src/main.asm
@@ -1,1 +1,46 @@
-.linecont       +               ; Allow line continuations.feature        c_comments      /* allow this style of comment */.segment "IMG".incbin "../assets/tiles/game_tiles.chr".include "./define/header.asm".include "./lib/utils.asm".include "./lib/gamepad.asm".include "./lib/ppu.asm".include "./define/palette.asm".include "./interrupt/irq.asm"              ; not currently using irq code, but it must be defined.include "./interrupt/reset.asm"            ; code and macros related to pressing the reset button.include "./interrupt/nmi.asm"game_loop:    lda nmi_ready    bne game_loop    jsr set_gamepad    set nmi_ready, #1    inc scroll_y    lda scroll_y        cmp #240    bne not_240        set scroll_y, #0    not_240:		jmp game_loop
+.linecont       +               ; Allow line continuations
+.feature        c_comments      /* allow this style of comment */
+
+.segment "IMG"
+.incbin "../img/HelloWorld.chr"
+
+.include "./def/header.asm"
+.include "./lib/utils.asm"
+.include "./lib/gamepad.asm"
+.include "./def/ppu.asm"
+.include "./def/palette.asm"
+
+.include "./vectors/irq.asm"              ; not currently using irq code, but it must be defined
+.include "./vectors/reset.asm"            ; code and macros related to pressing the reset button
+.include "./vectors/nmi.asm"
+
+
+game_loop:
+    lda nmi_ready
+    bne game_loop
+
+    jsr set_gamepad
+
+    set nmi_ready, #1
+
+    inc scroll_y
+    lda scroll_y
+
+    cmp #240
+    bne not_240
+        set scroll_y, #0
+    not_240:
+
+    jmp game_loop
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
